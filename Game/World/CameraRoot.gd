913710e2f220833a17f8344db913a539abef1649
Stefano Corno <stlecorno@gmail.com>
2024-08-19 17:05:06 -0700

    messing around with camera movement

--- Game/World/CameraRoot.gd
+++ Game/World/CameraRoot.gd
@@ -3,12 +3,21 @@ extends Node3D
 var rotational_velocity : float = 0
 @export var max_rotational_velocity : float
 
+var target_camera_position : int = 0
+@export var max_camera_speed : float = 5
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta : float) -> void:
 	rotational_velocity *= 0.93
-
 	rotate_y(rotational_velocity * delta)
+	
+	position.y = Global.FrameIndependentDamping(position.y, float(target_camera_position), 0.1, delta)
 
 func _input(event : InputEvent) -> void:
 	if (event.is_action_pressed("RotateCameraLeft")): rotational_velocity = -max_rotational_velocity
 	elif (event.is_action_pressed("RotateCameraRight")): rotational_velocity = max_rotational_velocity
+	
+	elif (event.is_action_pressed("MoveCameraUp")):
+		if (target_camera_position < Global.MAX_BLOCK_HEIGHT): target_camera_position += 1
+	elif (event.is_action_pressed("MoveCameraDown")):
+		if (target_camera_position > 0): target_camera_position -= 1
--- Game/World/Global.gd
+++ Game/World/Global.gd
@@ -3,15 +3,34 @@ extends Node
 const MAX_BLOCK_HEIGHT : int = 30
 const BLOCKS_PER_SIDE : int = 5
 
+var b_drag_mouse : bool
+var delta_mouse : Vector2 = Vector2.ZERO
+var last_mouse_pos : Vector2
+
+func _process(delta : float) -> void:
+	if (b_drag_mouse):
+		var mouse_pos : Vector2 = get_viewport().get_mouse_position()
+		delta_mouse = mouse_pos - last_mouse_pos
+		last_mouse_pos = mouse_pos
+
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("MouseDrag")): 
+		b_drag_mouse = true
+		last_mouse_pos = get_viewport().get_mouse_position()
+	elif (event.is_action_released("MouseDrag")): b_drag_mouse = false
+
+func lerp(source, target, factor : float):
+	return source * factor + target * (1 - factor)
+
 # can be used for doubles or vectors
-func FrameIndependentLerp(a, b, damping_factor : float, delta : float):
-	var factor = exp(log(damping_factor) * delta)
-	return a * factor + b * (1 - factor)
+func FrameIndependentDamping(source, target, damping_factor : float, delta : float):
+	var factor : float = pow(damping_factor, delta)
+	return lerp(source, target, factor)
 
 # float implementation
-func FrameIndependentClampedLerp(a : float, b : float, damping_factor : float, delta : float, max_step : float):
+func FrameIndependentClampedDamping(a : float, b : float, damping_factor : float, delta : float, max_step : float):
 	if (abs(b - a) >= max_step): return a + max_step * sign(b - a)
-	return FrameIndependentLerp(a, b, damping_factor, delta)
+	return FrameIndependentDamping(a, b, damping_factor, delta)
 	
 # vec implementation
 #func FrameIndependentClampedLerpVector(a: Vector3, b: Vector3, damping_factor: float, delta: float, max_step: float):
